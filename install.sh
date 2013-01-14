574a14f84f772d27b08ed585abad964bd5126c4a
Paul <paul.furley@dunelm.org.uk>
2013-01-14 22:09:05 +0000

    Did some fixes in the install script.
    When the script finishes it adds some instructions to README.md
    which is helpful for the user.

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ APP=$3
 cd $REPO
 git init
 
-echo "# README for $MYPROJECT" >> README.md
+echo "# README for $PROJECT" >> README.md
 git add README.md
 git commit -m "First commit, added README file."
 
@@ -52,8 +52,8 @@ mkdir $PROJECT/apps $PROJECT/libs
 mkdir $PROJECT/settings
 mv $PROJECT/settings.py $PROJECT/settings/settings.vanilla.py
 cp $SKEL_DIR/project/settings/*.py $PROJECT/settings/
-git add $PROJECT
-git commit -m "Added project $MYPROJECT."
+git add manage.py $PROJECT
+git commit -m "Added project $PROJECT."
 
 # copy skeleton apps and libraries into project
 cp -R $SKEL_DIR/project/apps/* $PROJECT/apps/
@@ -64,19 +64,37 @@ git commit -m "Added skeleton libs and apps."
 # create our first app
 mkdir $PROJECT/apps/$APP
 python manage.py startapp $APP $PROJECT/apps/$APP
-
-# replace MYAPP with app name in settings: INSTALLED_APPS etc
-sed -Ri "s/MYAPP/$APP/g" .
-sed -Ri "s/MYPROJECT/$PROJECT/g" .
 git add $PROJECT/apps/$APP
 git commit -m "Added app $APP."
+
+# replace MYAPP with app name in settings: INSTALLED_APPS etc
+find $PROJECT/settings/ -exec sed -i "s/MYAPP/$APP/g" {} \;
+find $PROJECT/settings/ -exec sed -i "s/MYPROJECT/$PROJECT/g" {} \;
+git add $PROJECT
+git commit -m "Updated settings for project $PROJECT and app $APP."
 
 # use the new settings module (dev)
 export DJANGO_SETTINGS_MODULE=$PROJECT.settings.dev
 
 # perform initial migration for app
+python manage.py syncdb
 python manage.py schemamigration $APP --initial
-python manage.py migrate $APP
 git add $PROJECT/apps/$APP/migrations
 git commit -m "Initial migration for $APP."
+
+python manage.py migrate $APP
+
+echo >> README.md
+echo "All changes are recorded in a git branch 'install'. The simplest thing" >> README.md
+echo "to do now is run the following commands:" >> README.md
+echo '$ git checkout master' >> README.md
+echo '$ git merge install' >> README.md
+echo '$ git branch -d install' >> README.md
+echo >> README.md
+echo "You can run your new installation with the following commands:" >> README.md
+echo "source venv/bin/activate" >> README.md
+echo "\$ export DJANGO_SETTINGS_MODULE=$PROJECT.settings.dev" >> README.md
+echo '$ python manage.py runserver' >> README.md
+
+cat README.md
 
